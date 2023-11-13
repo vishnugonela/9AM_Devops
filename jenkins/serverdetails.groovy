0b3431a1b6fd71d186f0f21b3198d87878a974c1
Nagakundana <149668347+Nagakundana@users.noreply.github.com>
2023-11-13 18:17:27 +0530

    Update serverdetails.groovy

--- jenkins/serverdetails.groovy
+++ jenkins/serverdetails.groovy
@@ -8,7 +8,7 @@ pipeline {
                     def masterNodeDetails = masterNode.toComputer()
                     echo "Master Node Name: ${masterNodeDetails.name}"
                     echo "Master Node Description: ${masterNode.nodeDescription}"
-                    echo "Master Node Labels: ${masterNode.nodeLabelString}"
+                    
                 }
             }
         }
