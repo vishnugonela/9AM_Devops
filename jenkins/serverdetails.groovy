4a5d78efba7f1aea4b9c28d2b64f9f3f98af4eeb
Nagakundana <149668347+Nagakundana@users.noreply.github.com>
2023-11-13 18:40:14 +0530

    Update serverdetails.groovy

--- jenkins/serverdetails.groovy
+++ jenkins/serverdetails.groovy
@@ -10,6 +10,22 @@ pipeline {
                     echo "Master Node Description: ${masterNode.nodeDescription}"
                     
                 }
+            stage('exceptional handling') {
+                steps {
+                    script{
+                        try {
+                        // Code that may throw an exception
+                        sh 'command_that_might_fail'
+                        
+                        // Additional steps
+                        echo 'Success'
+                    } catch (Exception e) {
+                        // Handle the exception
+                        echo "An error occurred: ${e.getMessage()}"
+                        
+                        // You can also mark the build as unstable or fail the build
+                        currentBuild.result = 'FAILURE'
+                    }
             }
         }
 }
