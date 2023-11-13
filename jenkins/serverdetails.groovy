b4c689af2abe1f98a30e6289e136ff120d257cba
Nagakundana <149668347+Nagakundana@users.noreply.github.com>
2023-11-13 18:06:02 +0530

    Update serverdetails.groovy

--- jenkins/serverdetails.groovy
+++ jenkins/serverdetails.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
     stages {
-        stage('Get Master Details') {
+        stage('Master Details') {
             steps {
                 script {
                     def masterNode = Jenkins.getInstance()
@@ -14,16 +14,6 @@ pipeline {
             }
         }
 
-        stage('Get Slave Details') {
-            steps {
-                script {
-                    for (slave in hudson.model.Jenkins.instance.slaves) {
-                        echo "Slave Node Name: ${slave.name}"
-                        echo "Slave Node Description: ${slave.nodeDescription}"
-                        echo "Slave Node Labels: ${slave.labelString}"
-                    }
-                }
-            }
-        }
+    
     }
 }
