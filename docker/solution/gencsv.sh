28d8a3ede17192ca1ffcbcb2244dd9b6eca8ffe9
Bhavyashree <Bhavyashree2201036@gmail.com>
2025-11-17 13:26:31 +0000

    Added gencsv.sh file to complete infracloudscvserver task

---
+++ docker/solution/gencsv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#########################
+# assigning dynamic variable values to contant variables 
+# ##########################################################
+start=$1
+end=$2 
+for ((i=start;i<=end;i++));do
+echo " Index:$i"
+echo "0, 234
+1, 98
+#2, 34">> inputFile
+mv inputFile inputdata
+done
