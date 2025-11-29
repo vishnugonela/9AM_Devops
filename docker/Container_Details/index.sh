98441f4c6b12aeb52c36a5a024a29ec12fd1aa1b
ANSHU KUMAR <140314710+ANSHUKRSAH@users.noreply.github.com>
2025-11-29 19:37:50 +0530

    Add script to display container details in HTML

---
+++ docker/Container_Details/index.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+HOSTNAME=$(hostname)
+IP=$(hostname -i)
+
+cat <<EOF > /usr/share/nginx/html/index.html
+<html> <body> <h2>Container Details</h2> <p>
+<b>Container Name:</b> $HOSTNAME</p> 
+<p><b>Container IP:</b> $IP</p> 
+</body> </html> 
+EOF
+
+exec nginx -g "daemon off;"
